be0585665f26e3a76cebd9829e07cd0376368c35
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-10-18 10:34:56 +0530

    add new js files

---
+++ programs/practiceSet1.js
@@ -0,0 +1,5 @@
+// Question1
+let str = "Sappu";
+let b = 3422;
+
+console.log(str + b); // concatnation
---
+++ programs/primitiveDataType_Objects.js
@@ -0,0 +1,27 @@
+// nn ss bb u - Premitive data types
+let a = null;
+let b = 123;
+let c = "abs";
+let d = Symbol("I am a good symbol");
+let e = true; // can also be false
+let f = BigInt("112") + BigInt("8");
+let g = undefined;
+// console.log(a, b, c, d, e, f, g);
+
+
+// Obvjects in JavaScript
+const item = {
+    "sappu" : "book",
+    "Rohan" : "pen",
+    "Lavish" : 123,
+    "Shubh" : false,
+    "Harry" : true,
+    "sangam" : undefined
+}
+
+console.log(item["sangem"]);
+console.log(item["Rohan"]);
+console.log(item["Lavish"]);
+console.log(item["Harry"]);
+console.log(item["Shubh"]);
+console.log(item["sappu"]);
