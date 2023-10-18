eca066a05bf1e619135f1e9c989b33fb6c6c146a
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-10-18 21:13:10 +0530

    add new js file

---
+++ programs/Operators.js
@@ -0,0 +1,17 @@
+// Arithmetic Operators
+let a = 10;
+let b = 2;
+
+console.log("a+b = ", a+b);
+console.log("a-b = ", a-b);
+console.log("a*b = ", a*b);
+console.log("a/b = ", a/b);
+console.log("a**b = ", a**b);
+
+// Logical Operators
+let x =true;
+let y = false;
+
+console.log("x && y =", x && y); // logical AND
+console.log("x || y =", x || y); // logical OR
+console.log("!x =", !x); // logical NOT
