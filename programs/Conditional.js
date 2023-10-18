7caf5ddbfc8c392068f3320cf510b3ac79ec77e5
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-10-18 21:57:35 +0530

    add new js file

---
+++ programs/Conditional.js
@@ -0,0 +1,8 @@
+let a = prompt();
+let b = prompt();
+
+if (a > b) {
+    console.log("a is greater than b");
+} else {
+    console.log("b is greater than a");
+}
--- programs/Operators.js
+++ programs/Operators.js
@@ -8,10 +8,36 @@ console.log("a*b = ", a*b);
 console.log("a/b = ", a/b);
 console.log("a**b = ", a**b);
 
+console.log("a++ = ", a++);
+console.log("a-- = ", a--);
+console.log("++a = ", ++a);
+console.log("--a = ", --a);
+
+// Assignment Operators
+console.log("a += 10: ", a += 10);
+console.log("a -= 10: ", a -= 10);
+console.log("a *= 10: ", a *= 10);
+console.log("a /= 10: ", a /= 10);
+console.log("a %= 10: ", a %= 10);
+
 // Logical Operators
 let x =true;
 let y = false;
-
 console.log("x && y =", x && y); // logical AND
 console.log("x || y =", x || y); // logical OR
 console.log("!x =", !x); // logical NOT
+
+// Comparison Operators
+console.log("a == b: ", a == b);
+console.log("a != b: ", a != b);
+console.log("a > b: ", a > b);
+console.log("a < b: ", a < b);
+console.log("a >= b: ", a >= b);
+console.log("a <= b: ", a <= b);
+console.log("a === b: ", a === b);
+console.log("a!== b: ", a !== b);
+
+// Logical Oporators
+console.log("a && b: ", a && b);
+console.log("a || b: ", a || b);
+console.log("!a: ", !a)
