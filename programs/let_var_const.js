631c4cf559a251093290fca6070ea0301edeed16
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-10-18 09:47:15 +0530

    add new js file

---
+++ programs/let_var_const.js
@@ -0,0 +1,17 @@
+console.log("Let's go for the ...hunt!");
+
+let name = "sapnendra";
+const author = "Sapnendra Jaiswal";
+// author = "Sappu";   // It will thows an error because of constant variables
+// console.log(author);
+
+let b = 10;
+{
+    let b = 20;
+    // console.log(b);
+}
+
+// console.log(b);
+
+let x = undefined;
+console.log(x);
