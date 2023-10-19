a16a24c41181276b00b777a84b8d729a15a17299
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-10-19 21:14:55 +0530

    add new js file

---
+++ programs/practiceSet2.js
@@ -0,0 +1,73 @@
+// Practice set 2
+
+// question 1
+/*
+let age = prompt("Enter your age");
+
+age = Number.parseInt(age);
+
+if(age > 10 && and < 20) {
+    console.log("your age is between 10 to 20");
+} else {
+    console.log("Your age doesn't lie between 10 to 20");
+} 
+*/
+
+// question 2
+
+/*
+let a = prompt("Enter a number");
+a = Number.parseInt(a);
+switch(a) {
+    case 1:
+        console.log("You Enter 1.");
+        break;
+    case 2:
+        console.log("You Enter 2.");
+        break;
+    case 3:
+        console.log("You Enter 3.");
+        break;
+    case 4:
+        console.log("You Enter 4.");
+        break;
+    case 5:
+        console.log("You Enter 5.");
+        break;
+    case 6:
+        console.log("You Enter 6.");
+        break;
+    case 7:
+        console.log("You Enter 7.");
+        break;
+    case 8:
+        console.log("You Enter 8.");
+        break;
+    case 9:
+        console.log("You Enter 9");
+        break;
+    case 10:
+        console.log("You Entered last number in your range.");
+        break;
+    default:
+        console.log("Your number doesn't lie between 1 to 10.");
+        return;
+}
+*/
+
+// Question 3
+/*
+let num = 1;
+
+if(num%2 == 0 && num%3 == 0) {
+    console.log("Your number is divisible by 2 and 3.");
+} else {
+    console.log("Your number is not divisible by 2 and 3.");
+}
+*/
+
+// Question 4
+/*
+let a = 19;
+a > 18 ? console.log("You can drive") : console.log("You can't drive");
+*/
