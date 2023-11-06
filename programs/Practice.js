ed033dd166b6ceed23f453a3ada1359f962fcd0b
Sapnendra Jaiswal <sapnendrajaiswal2388@gmail.com>
2023-11-06 10:58:35 +0530

    add new js file

---
+++ programs/Practice.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync")();
+
+let age = prompt("Enter an integer: ");
+age = parseInt(age);
+
+let check = age > 18 ? "You can drive" : "You can't drive";
+console.log(check);
+
--- programs/practice.js
+++
@@ -1,9 +0,0 @@
-const prompt = require("prompt-sync")();
-
-const words = {
-    apple: "A famous fruit",
-    boy: "A boy have age is<18",
-    cat: "Billi"
-}
-
-console.log(words.boy);
